b7581207dff49e57560c854051ddefebdd0b91a7
ryupatterson <54785861+ryupatterson@users.noreply.github.com>
2022-04-13 23:53:43 -0400

    first commit

---
+++ database_connection.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Author(s): Ryu Patterson, Maven Kim, Quang Lam
+ */
+
+/*
+ * $dbserver = "localhost";
+ * $dbuser = "rjp5cc";
+ * $dbpass = "Winter2022!!";
+ * $dbdatabase = "rjp5cc";
+ */
+
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbdatabase = "cs4640";
